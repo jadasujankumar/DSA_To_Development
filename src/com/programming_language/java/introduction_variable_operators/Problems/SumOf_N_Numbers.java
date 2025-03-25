f333d1f77352176f838f14e1062221323f202aa5
sujankumarjada <jadasujankumar@gmail.com>
2025-03-25 23:06:47 +0530

    init commit

---
+++ src/com/programming_language/java/introduction_variable_operators/Problems/SumOf_N_Numbers.java
@@ -0,0 +1,13 @@
+package com.programming_language.java.introduction_variable_operators.Problems;
+
+public class SumOf_N_Numbers {
+
+    public static void main(String[] args) {
+        int n = 10;
+
+        int sum = n*(n+1)/2;
+
+        System.out.println("Sum of first "+n+" natural numbers are : "+sum);
+
+    }
+}
