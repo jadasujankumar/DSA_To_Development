04e350ccead48137d0a871fcfadfcd2f66d7d7c5
sujankumarjada <jadasujankumar@gmail.com>
2025-03-25 22:47:43 +0530

    init commit

---
+++ src/SwapTwoNumbers.java
@@ -0,0 +1,18 @@
+public class SwapTwoNumbers {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int temp;
+
+        System.out.println("The Values of A and B before swapping are : a : "+a+" b : "+b);
+
+        temp = a;
+
+        a=b;
+        b=temp;
+
+        System.out.println("The Values of A and B after swapping are : a : "+a+" b : "+b);
+
+    }
+}
