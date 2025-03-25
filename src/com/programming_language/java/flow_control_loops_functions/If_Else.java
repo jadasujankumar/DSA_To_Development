a9eb50e66dfb30cdcef14a957f6fbacbec96d239
sujankumarjada <jadasujankumar@gmail.com>
2025-03-25 23:12:51 +0530

    init commit

---
+++ src/com/programming_language/java/flow_control_loops_functions/If_Else.java
@@ -0,0 +1,17 @@
+package com.programming_language.java.flow_control_loops_functions;
+
+public class If_Else {
+
+    public static void main(String[] args) {
+        int age = 13;
+
+        if(age>=18)
+        {
+            System.out.print("You are eligible to vote");
+        }
+        else
+        {
+            System.out.print("You are not eligible to vote");
+        }
+    }
+}
