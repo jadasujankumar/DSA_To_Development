9f35524813a952d35ad5b212a6f1b6c8405f2d34
sujankumarjada <jadasujankumar@gmail.com>
2025-03-27 09:55:37 +0530

    init commit

---
+++ src/com/programming_language/java/introduction_variable_operators/RelationalOperators.java
@@ -0,0 +1,17 @@
+package com.programming_language.java.introduction_variable_operators;
+
+import java.util.*;
+
+public class RelationalOperators {
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 7;
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+    }
+}
