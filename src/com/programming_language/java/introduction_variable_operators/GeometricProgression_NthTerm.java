7f7ac25f50ae77fd526d90f210c2808c0f322387
sujankumarjada <jadasujankumar@gmail.com>
2025-03-27 20:09:27 +0530

    init commit

--- src/com/programming_language/java/introduction_variable_operators/GeometricProgression_NthTerm.java
+++ src/com/programming_language/java/introduction_variable_operators/GeometricProgression_NthTerm.java
@@ -1,5 +1,24 @@
 package com.programming_language.java.introduction_variable_operators;
 
+import java.util.Scanner;
+
 public class GeometricProgression_NthTerm {
 
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first term in Gp : ");
+        int a = input.nextInt();
+
+        System.out.print("Enter the common ratio of the Gp : ");
+        int r = input.nextInt();
+
+        System.out.print("Enter the Nth term you want to find in Gp : ");
+        int n = input.nextInt();
+
+        double nthTermInGp = (a*Math.pow(r,n-1));
+
+        System.out.print(nthTermInGp);
+    }
 }
