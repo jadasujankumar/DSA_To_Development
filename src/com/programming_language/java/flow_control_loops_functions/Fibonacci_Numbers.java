dce906a8fdf9a477246e574f2b34560b607982b1
sujankumarjada <jadasujankumar@gmail.com>
2025-03-31 17:21:57 +0530

    init commit

---
+++ src/com/programming_language/java/flow_control_loops_functions/Fibonacci_Numbers.java
@@ -0,0 +1,25 @@
+package com.programming_language.java.flow_control_loops_functions;
+
+import java.util.Scanner;
+
+public class Fibonacci_Numbers {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number n : ");
+        int n = input.nextInt();
+
+        int a =1,b=1;
+        int c=0;
+        System.out.print(a+" "+b+" ");
+        for(int i =2;i<=n;i++)
+        {
+            c = a+b;
+            System.out.print(c+" ");
+            a = b;
+            b = c;
+        }
+    }
+}
