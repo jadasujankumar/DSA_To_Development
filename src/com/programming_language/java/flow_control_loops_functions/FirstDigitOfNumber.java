ad737e8f2fe5da6f518b8d23fbbb6f1fe750edf7
sujankumarjada <jadasujankumar@gmail.com>
2025-03-31 18:27:31 +0530

    init commit

--- src/com/programming_language/java/flow_control_loops_functions/FirstDigitOfNumber.java
+++ src/com/programming_language/java/flow_control_loops_functions/FirstDigitOfNumber.java
@@ -1,8 +1,20 @@
 package com.programming_language.java.flow_control_loops_functions;
 
+import java.util.Scanner;
+
 public class FirstDigitOfNumber {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number ");
+        int num = input.nextInt();
+
+        while(num > 10)
+        {
+            num = num/10;
+        }
+        System.out.print("First Digit : "+num);
     }
 }
